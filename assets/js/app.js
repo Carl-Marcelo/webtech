22948b684041e72b9e8009d60be9b1a3c330510e
Carl Marcelo <carlmarcelo26@yahoo.com>
2023-03-17 18:44:32 +0800

    responsive added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+// social media icons
 $(document).ready(function(){
 	var socialButton = $('.button__social');
 
@@ -13,8 +14,37 @@ $(document).ready(function(){
 	});
 });
 
+// slider option
 $(document).ready(function(){
     $('.slider').bxSlider({
 		pager: false
+	});
+});
+
+// hide header nav if window is less than 980px
+$(window).on('load', function() {
+	if ($(window).width() < 980) {
+		$('header nav').slideUp();
+	} else {
+		$('header nav').show();
+	}
+});
+
+$(document).ready(function() {
+	var open__nav = $('.open__nav');
+	var nav__wrap = $('.nav__wrap');
+	var nav = $('header nav');
+
+	$(open__nav).on('click', function() {
+		$(this).removeAttr('href');
+		$(this).toggleClass('open');
+
+		$(nav__wrap).toggleClass('nav__open');
+
+		if($(nav__wrap).hasClass('nav__open')) {
+			$(nav).slideDown();
+		} else {
+			$(nav).slideUp();
+		}
 	});
 });
